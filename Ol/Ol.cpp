e008727c9b918d5e9bc613b1fb05e4b42e19566d
Sara894 <t48250klo@gmail.com>
2022-03-12 14:05:47 +0300

    вторая задача

--- Ol/Ol.cpp
+++ Ol/Ol.cpp
@@ -3,63 +3,43 @@
 
 //Требуется узнать максимальное число, сумма цифр в десятичной записи которого равна n,
 //а также в его десятичной записи нет нулей и подряд идущих одинаковых цифр.
+//https://codeforces.com/contest/1647/problem/B
 #include <iostream>
 using namespace std;
-int sumCifr(int);
-int numRazrad(int n);
+
 int main()
 {
-   /* int n;
-    cin >> n;*/
-    
-    //нолей нет вообще
-    //подряд идущие одинаковые цифры нельзя
-    //чем меньше каждая цифра в разрядной сетке, тем больше само число
-    //т.е n = 5 то можно 5, 14,23 ,32,41,212
-    // 0<=n<=1000
-    int sum = 0;
-  
-   /* for (int i = 1; i < n && sum != n; i++)
-    {
-        sum += i;
-        cout << i;
-    }*/
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;//размеры таблицы
+		cin >> n >> m;
+		bool elegant =1;
+		/*if (elegant)
+			cout << "YES";
+		else
+			cout << "NO";*/
+		int** arr = new int *[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = new int[m];
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				printf("\n arr[%d][%d]: ", i, j);
+				cin >> arr[i][j];
+			}
+		}
 
-    //for (int i = 1; sumCifr(i) <= 2 ; i++)
-    //{
-    //    cout << i << endl;
-    //}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				cout << arr[i][j] << " ";
+			}
+			cout << "\n";
+		}
 
-    numRazrad(7);
+	}
 }
-
-int sumCifr(int cifr)
-{
-   
-    int sum = 0;
-    while (cifr != 0)
-    {
-        sum += cifr % 10;
-        cifr /= 10;
-    }
-    return sum;
-
-}
-
-int numRazrad(int n) {//сколько разрядов у числа сумма цифер которого примерно равна n
-    int numRazrad = 0;
-    if (n == 1)
-        numRazrad = 1;
-    else
-    {
-        int i = 1;
-        while (n > 0) {
-            n -= i;
-            cout << n<<endl;
-            i++;
-        }
-    }
-
-    return numRazrad;
-}
-
